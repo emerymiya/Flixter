f15bed3287fb5df54dd94a591f3dcd6cdab847c1
Emery Green <emerymiya@gmail.com>
2019-11-21 04:11:09 +0000

    Lesson 15 halfway finished

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -12,11 +12,15 @@ class Instructor::CoursesController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
-  end
 
   def show
     @course = Course.find(params[:id])
   end
+
+  def index
+    @courses = Course.all
+  end
+end
 
   private
 
